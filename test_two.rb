7a3f84452f67af13ed5a2c5247d03cb672b1b9e8
Charlie Barth <charliepbarth@gmail.com>
2020-06-24 16:27:35 +0000

    Automatically backed up by Learn

--- test_two.rb
+++ test_two.rb
@@ -1,13 +1,6 @@
 
-def line(katz_deli)
-  if katz_deli.size >= 1
-    count = katz_deli.size - 1
-    puts "The line is currently:  
-    
-  else 
-     puts "The line is currently empty."
-  
-      
-  end
-end
-line(["Logan", "Avi", "Spencer"])
+
+line = ["Logan", "Avi", "Spencer"]
+maybe = line.shift
+puts maybe
+puts maybe.unshift
