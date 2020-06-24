bdbfc548bd196b3162494d1e38f71d7ff18a4a74
Charlie Barth <charliepbarth@gmail.com>
2020-06-24 16:09:05 +0000

    Automatically backed up by Learn

--- test_two.rb
+++ test_two.rb
@@ -3,5 +3,5 @@ katz_deli = ["Logan", "Avi", "Spencer"]
  count = katz_deli.size - 1
     puts "The line is currently: #{katz_deli[0..count]}"
     katz_deli.each do |name|
-      puts "Hi my name is#{name}."
+      puts "Hi my name is #{name}."
     end
