e8e53e6fad9278d61afccff8ec25930dad29f6f2
Charlie Barth <charliepbarth@gmail.com>
2020-06-24 16:16:22 +0000

    Automatically backed up by Learn

--- test_two.rb
+++ test_two.rb
@@ -1,1 +1,13 @@
-katz_deli = ["Logan", "Avi", "Spencer, "]
+
+def line(katz_deli)
+  if katz_deli.size >= 1
+    count = katz_deli.size - 1
+    puts "The line is currently: #{katz_deli[0..count]}"
+    elsif katz_deli.size == 0
+     puts "The line is currently empty."
+  else
+      
+  end
+end
+line(["Logan", "Avi", "Spencer"])
+katz_deli(["Logan", "Avi", "Spencer, "])
