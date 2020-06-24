8a5bad07149dd3f5c452ac77cbc0a30958f60564
Charlie Barth <charliepbarth@gmail.com>
2020-06-24 16:19:48 +0000

    Automatically backed up by Learn

--- test_two.rb
+++ test_two.rb
@@ -5,9 +5,9 @@ def line(katz_deli)
     maybe = katz_deli.each do
     puts "The line is currently:" + 
     
-    elsif katz_deli.size == 0
+    else katz_deli.size
      puts "The line is currently empty."
-  else
+  
       
   end
 end
