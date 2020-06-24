d6b45ce6796e2ea826c8adfb0dbf83b8f3d2b584
Charlie Barth <charliepbarth@gmail.com>
2020-06-24 15:52:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,6 @@
 def line(katz_deli)
   if katz_deli.size >= 1
-    counter = 1
-    katz_deli.each do |name|
-      puts "Welcome, #{name}. You are currently number #{counter} in line."
-      counter += 1
+    
     end
     elsif katz_deli.size == 0
      puts "The line is currently empty."
