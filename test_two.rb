ff9cff2670e999d1789c123a9237f2324369cb77
Charlie Barth <charliepbarth@gmail.com>
2020-06-24 16:08:00 +0000

    Automatically backed up by Learn

--- test_two.rb
+++ test_two.rb
@@ -3,4 +3,4 @@ array = ["Logan", "Avi", "Spencer"]
  count = katz_deli.size - 1
     puts "The line is currently: #{katz_deli[0..count]}"
     katz_deli.each do |name|
-      katz_deli
+      puts "#{|name|}"
