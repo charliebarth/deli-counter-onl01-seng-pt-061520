2cef6c5a8da2e4b9c872e0c50ace6e124010917c
Charlie Barth <charliepbarth@gmail.com>
2020-06-24 16:21:02 +0000

    Automatically backed up by Learn

--- test_two.rb
+++ test_two.rb
@@ -3,7 +3,7 @@ def line(katz_deli)
   if katz_deli.size >= 1
     count = katz_deli.size - 1
     maybe = katz_deli.each do
-    puts "The line is currently:" + 
+    puts "The line is currently:" 
     
     else 
      puts "The line is currently empty."
