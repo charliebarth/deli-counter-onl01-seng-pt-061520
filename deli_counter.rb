5add13e4b229a075a98930bda421ca9d70b2884d
Charlie Barth <charliepbarth@gmail.com>
2020-06-24 15:53:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,6 @@ def line(katz_deli)
   if katz_deli.size >= 1
     count = katz_deli.size - 1
     puts "The line is currently: #{katz_deli[0..count]}"
-    end
     elsif katz_deli.size == 0
      puts "The line is currently empty."
   else
