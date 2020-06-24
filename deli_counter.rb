041c91b48338d487a196a4ae8ba2f1c3b3e71831
Charlie Barth <charliepbarth@gmail.com>
2020-06-24 17:15:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,11 @@
 def line(katz_deli)
   if katz_deli.size >= 1
-    line = ["Logan", "Avi", "Spencer"]
     count = 0
     num = 0
     new_array = []
-    line.size.times do
+    katz_deli.size.times do
       count += 1
-      next_customer = "#{count}. " + "#{line[num]}"
+      next_customer = "#{count}. " + "#{katz_deli[num]}"
       num += 1
       new_array.push(next_customer)
 end
